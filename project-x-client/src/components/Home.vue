927a179ac6ef02970e68b4e9e89c88b6dcbcd34c
Avinash Aerra <avi6105@gmail.com>
2020-05-08 02:19:26 -0700

    Making GET request but need to make server for CORS

--- project-x-client/src/components/Home.vue
+++ project-x-client/src/components/Home.vue
@@ -1,14 +1,58 @@
 <template>
-    <v-col cols="12" sm="6">
-        <v-text-field
-        label="Search"
-        id="search"
-        append-icon="search"
-        solo
-        ></v-text-field>
-    </v-col>
+  <v-form ref="form" lazy-validation>
+    <v-container class="item-search">
+        <v-row>
+          <v-text-field v-model="item" class="item-search-field" type="text" placeholder="Item.."
+          solo> </v-text-field>
+          <v-btn v-on:click="search" class="item-search-button" height="48px" color="red">
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+        </v-row>
+    </v-container>
+    <div v-if="loading">
+        <v-icon>mdi-spin</v-icon>
+    </div>
+    <div v-else>{{results}}</div>
+  </v-form>
 </template>
 
 <style lang="scss" scoped>
-@import "../styles/Home.scss";
+@import "../styles/home.scss";
 </style>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  props: {
+    results: null,
+  },
+  data() {
+    return {
+      item: '',
+      loading: false,
+    };
+  },
+  methods: {
+    search() {
+      // Do actual search request
+      console.log(this.item);
+      this.loading = true;
+      this.requestUrl = 'https://rsbuddy.com/exchange/graphs/1440/';
+      axios
+        // eslint-disable-next-line prefer-template
+        .get(this.requestUrl + this.item + '.json')
+        .then((response) => {
+          this.results = response;
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+  },
+};
+</script>
