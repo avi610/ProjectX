8ceb001b669304830a0a70c411bccf3916787f66
Avinash Aerra <avi6105@gmail.com>
2020-07-06 16:07:30 -0700

    changes

--- project-x-server/src/bin/routes/items.js
+++ project-x-server/src/bin/routes/items.js
@@ -5,16 +5,23 @@ var request = require('request');
 const Logger = require('../services/loggerService');
 const logger = new Logger('app')
 
+router.get('/', function(req, res, next) {
+  
+});
+
 router.get('/item/:id', function(req, res, next) {
   itemId = req.params.id;
   baseUrl = 'http://services.runescape.com/m=itemdb_oldschool';
   itemUrl = '/api/catalogue/detail.json?item=';
   
-  logger.info('url', baseUrl + itemUrl + itemId);
+  //logger.info('Sending ', baseUrl + itemUrl + itemId);
 
   request(baseUrl + itemUrl + itemId , function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred and handle it
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    if (error) {
+      console.log('error:', error); // Print the error if one occurred and handle it
+    }
+    //logger.info()
+    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     res.send(body)
   });
 });
